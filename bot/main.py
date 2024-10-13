6ccaff0a5c45a845a94abe3addcb4310a209d9a6
Denis Karpov <karpovdenis.al@gmail.com>
2024-10-13 15:37:12 +0300

    Create sample echo bot

---
+++ bot/main.py
@@ -0,0 +1,29 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+dp = Dispatcher(storage=MemoryStorage())
+
+
+@dp.message(F.text)
+async def echo(message: types.Message):
+    await message.reply(message.text)
+
+
+async def main():
+    await dp.start_polling(bot, skip_updates=True)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
