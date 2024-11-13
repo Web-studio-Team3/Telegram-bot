cc324dede901f9e57b595e4787df92d45082444a
JengoWins <unreal.gold@mail.ru>
2024-11-13 16:02:58 +0300

    Обновили модели Объявлений и вывод на консоль.

--- bot/ads/model.py
+++ bot/ads/model.py
@@ -1,12 +1,23 @@
 """Модель данных для объявдения"""
-from pydantic import BaseModel
+#from pydantic import BaseModel
+from typing import Optional
 
 
-class Ads(BaseModel):
-    id: int
-    id_user: str
-    contact: str
-    description: str
-    photo: bytes
-    price: int
-    anonim: str
+class Ads:
+    id: Optional[int] = None
+    id_user: Optional[str] = None
+    contact: Optional[str] = None
+    description: Optional[str] = None
+    photo: Optional[bytes] = None
+    price: Optional[int] = None
+    anonim: Optional[str] = None
+
+    def __str__(self):
+        return f"id: {self.id}\nid_user: {self.id_user}\ncontact: {self.contact}\ndescription: { self.description}\n photo: {self.photo}\nanonim: {self.anonim}"
+    # def __init__(self):
+    #     self.id = 1
+    #     self.id_user = "m"
+    #     self.contact = "1"
+    #     self.description= "1"
+    #     self.photo = bytes("010101")
+    #     self.anonim = "1"
--- bot/ads/router.py
+++ bot/ads/router.py
@@ -1,17 +1,22 @@
 """Роутер с хендлерами для обхявления"""
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.filters.callback_data import CallbackData
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from aiogram.utils.chat_action import ChatActionSender
+
 from config import redis_db
 from typing import Any
 from bot.ads.model import Ads
 from bot.ads.inline_buttons import inl_keyboard
 
-
 ads_router = Router(name=__name__)
+storage = MemoryStorage()
 
 
-@ads_router.message(Command("Объявление"))
+@ads_router.message(Command("hh"))
 async def message_handler(message: Message) -> Any:
     """
     Функция для создания объявления
@@ -23,11 +28,16 @@ async def message_handler(message: Message) -> Any:
 
     ads.id = message.message_id
     ads.id_user = message.from_user.id
-    ads.anonim = await message.answer("Объявление будет Анонимным?", reply_markup=inl_keyboard)
+    await message.answer("Объявление будет Анонимным?", reply_markup=inl_keyboard)
+    print(ads)
     # redis_db.set("test", f"{message.text}")
     # await message.answer(redis_db.get("test"))
     # redis_db.close()
 
+
+@ads_router.callback_query(Command("/re"))
+async def cmd_start(call: CallbackQuery, state: FSMContext):
+    await message
 
 # @ads_router.callback_query(func=lambda value: value.data == 'yes')
 # async def yes_callback():
