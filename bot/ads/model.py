ba573e56bebfc448101e77a6008dcacfcc270eff
Kwokazer <ipoteka13372281448@gmail.com>
2024-11-20 13:25:48 +0300

    Added __init__ constructor for the advertisement model to initialize parameters such as id, id_user, contact, description, photo, price, and anonim

--- bot/ads/model.py
+++ bot/ads/model.py
@@ -12,6 +12,24 @@ class Ads:
     price: Optional[int] = None
     anonim: Optional[str] = None
 
+    def __init__(
+            self,
+            id: Optional[int] = None,
+            id_user: Optional[str] = None,
+            contact: Optional[str] = None,
+            description: Optional[str] = None,
+            photo: Optional[bytes] = None,
+            price: Optional[int] = None,
+            anonim: Optional[str] = None,
+    ):
+        self.id = id
+        self.id_user = id_user
+        self.contact = contact
+        self.description = description
+        self.photo = photo
+        self.price = price
+        self.anonim = anonim
+
     def __str__(self):
         return f"id: {self.id}\nid_user: {self.id_user}\ncontact: {self.contact}\ndescription: { self.description}\n photo: {self.photo}\nanonim: {self.anonim}"
     # def __init__(self):
