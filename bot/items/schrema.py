77569c8b702bec5f058b3fa001801c6bbdf6eabb
Caesarqq <aszxclkh@gmail.com>
2024-11-26 00:02:15 +0300

    redis
    кэширование

--- bot/items/schrema.py
+++ bot/items/schrema.py
@@ -12,3 +12,11 @@ class Items(BaseModel):
     cost: str
     status: str
     seller_id: str
+
+    def __str__(self):
+        return (
+            f"🔹 <b>Название:</b> {self.title}\n"
+            f"🔹 <b>Описание:</b> {self.description}\n"
+            f"🔹 <b>Контакт:</b> {self.address}\n"
+            f"🔹 <b>Цена:</b> {self.cost} ₽"
+        )
